b7f30dc656c4f3414499c7e237941d614b63189d
Dominik Masur <dominik.masur@googlemail.com>
2010-12-08 22:40:52 +0100

    = form_for

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
-
 Rails.application.routes.draw do
   resources :wishlists
   resources :wished_products
   match '/wishlist', :controller => :wishlists, :action => 'show', :as => "default_wishlist"
   match '/wishlists/email_to_friend/:id', :controller => :wishlists, :action => 'email_to_friend', :as => "email_to_friend"
 end
-
-
